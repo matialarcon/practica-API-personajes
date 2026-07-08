a7bcb76a3f49d83da99124db7da071a42e38dcc1
Matías Alarcón <matiasalarcon069@gmail.com>
2026-07-08 16:17:36 -0300

    Creación del componente ListCharacters, en el cuál se fetchearon y renderizaron los personajes desde la API, mostrando su imagen y nombre. También se agregaron mas estilos para mostrar a los personajes en forma de tarjetas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react"
+
 function App() {
-  return (
+  return(
     <>
-      <Searcher />
+      <header>
+        <Searcher />
+      </header>
+
+      <section>
+        <ListCharacters />
+      </section>
     </>
   )
 }
@@ -12,6 +20,40 @@ function Searcher() {
       <form className="searcher-character-container">
         <input type="text" placeholder="Buscar personaje" className="searcher-character" name="character"/>
       </form>
+    </>
+  )
+}
+
+function ListCharacters() {
+  const [character, setCharacter] = useState([])
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    fetch("https://akabab.github.io/superhero-api/api/all.json")
+      .then(res => {
+        if (!res.ok) throw new Error(`Error ${res.status}`)
+        return res.json()
+      })
+      .then(data => setCharacter(data))
+      .catch(error => setError(error.message))
+  }, [])
+
+  if (error != "") return <p>{error}</p>
+
+  return (
+    <>
+      <h1>Lista de personajes</h1>
+
+      <div>
+        <ul className="character-list">
+          {character.map(p => (
+            <li key={p.id} className="character-container">
+              <img src={p.images.lg} alt="imagen personaje" className="character-img"/>
+              <p className="character-name">{p.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
